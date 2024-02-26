ecca9f6ef2a21e131a9b6a3ed74e639eb0d22c10
Dylan Sprague <ithilanor@gmail.com>
2024-02-26 01:44:55 +0000

    Change main() to load grids from file, then call solver

--- main.go
+++ main.go
@@ -2,47 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/DylanSp/sudoku-toolkit/sudoku"
 )
 
 func main() {
-	grid := sudoku.EmptyGrid(2)
+	currentWorkingDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 
-	fmt.Printf("Is an empty grid completely filled? (should be false): %v\n", grid.IsCompletelyFilled())
+	filename := filepath.Join(currentWorkingDir, "examples", "4x4", "filledGrid.txt")
 
-	// valid 4x4 sudoku solution:
-	/*
-			2 3 | 1 4
-			4 1 | 3 2
-			----|----
-			3 4 | 2 1
-		    1 2 | 4 3
-	*/
+	grids, err := sudoku.LoadGridsFromFile(filename)
+	if err != nil {
+		fmt.Printf("Unable to load grids from %v\n", filename)
+		fmt.Println(err)
+		panic(err)
+	}
 
-	grid.SetCellToElement(0, 0, 2)
-	grid.SetCellToElement(0, 1, 3)
-	grid.SetCellToElement(0, 2, 1)
-	grid.SetCellToElement(0, 3, 4)
-
-	grid.SetCellToElement(1, 0, 4)
-	grid.SetCellToElement(1, 1, 1)
-	grid.SetCellToElement(1, 2, 3)
-	grid.SetCellToElement(1, 3, 2)
-
-	grid.SetCellToElement(2, 0, 3)
-	grid.SetCellToElement(2, 1, 4)
-	grid.SetCellToElement(2, 2, 2)
-	grid.SetCellToElement(2, 3, 1)
-
-	grid.SetCellToElement(3, 0, 1)
-	grid.SetCellToElement(3, 1, 2)
-	grid.SetCellToElement(3, 2, 4)
-	grid.SetCellToElement(3, 3, 3)
-
-	fmt.Printf("Is the grid completely filled after setting values? (should be true): %v\n", grid.IsCompletelyFilled())
-	fmt.Printf("Is the solution valid? (should be true): %v\n", grid.IsValidSolution())
-
-	grid.SetCellToElement(3, 3, 4)
-	fmt.Printf("Is the solution valid after changing a value? (should be false): %v\n", grid.IsValidSolution())
+	for _, grid := range grids {
+		solvedGrid := sudoku.SolveWithBacktracking(grid)
+		fmt.Println(solvedGrid)
+	}
 }
